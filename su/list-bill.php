5627f1cf108321137f13c7f4af4a93a9f07f86bf
hienvu79 <65020525+hienvu79@users.noreply.github.com>
2020-12-29 22:12:15 +0700

    Update

--- su/list-bill.php
+++ su/list-bill.php
@@ -45,7 +45,6 @@ GROUP BY t1.room_id";
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">DANH SÁCH PHÒNG</h1>
-            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
           </div>
 
           <!-- Content Row -->
